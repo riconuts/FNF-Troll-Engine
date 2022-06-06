c9cc07520a2900328c0053b48971ff15b8ee197e
Echo <82014378+Echolocated@users.noreply.github.com>
2022-06-06 16:08:10 -0400

    Initial commit

---
+++ source/Conductor.hx
@@ -0,0 +1,110 @@
+package;
+
+import Song.SwagSong;
+
+/**
+ * ...
+ * @author
+ */
+
+typedef BPMChangeEvent =
+{
+	var stepTime:Int;
+	var songTime:Float;
+	var bpm:Float;
+}
+
+class Conductor
+{
+	public static var bpm:Float = 100;
+	public static var crochet:Float = ((60 / bpm) * 1000); // beats in milliseconds
+	public static var stepCrochet:Float = crochet / 4; // steps in milliseconds
+	public static var songPosition:Float=0;
+	public static var lastSongPos:Float;
+	public static var offset:Float = 0;
+
+	//public static var safeFrames:Int = 10;
+	public static var safeZoneOffset:Float = (ClientPrefs.safeFrames / 60) * 1000; // is calculated in create(), is safeFrames in milliseconds
+
+	public static var bpmChangeMap:Array<BPMChangeEvent> = [];
+
+	public function new()
+	{
+	}
+
+	public static function judgeNote(note:Note, diff:Float=0):Rating // die
+	{
+		var data:Array<Rating> = PlayState.instance.ratingsData; //shortening cuz fuck u
+		for(i in 0...data.length-1) //skips last window (Shit)
+		{
+			if (diff <= data[i].hitWindow)
+			{
+				return data[i];
+			}
+		}
+		return data[data.length - 1];
+	}
+
+	public static function mapBPMChanges(song:SwagSong)
+	{
+		bpmChangeMap = [];
+
+		var curBPM:Float = song.bpm;
+		var totalSteps:Int = 0;
+		var totalPos:Float = 0;
+		for (i in 0...song.notes.length)
+		{
+			if(song.notes[i].changeBPM && song.notes[i].bpm != curBPM)
+			{
+				curBPM = song.notes[i].bpm;
+				var event:BPMChangeEvent = {
+					stepTime: totalSteps,
+					songTime: totalPos,
+					bpm: curBPM
+				};
+				bpmChangeMap.push(event);
+			}
+
+			var deltaSteps:Int = song.notes[i].lengthInSteps;
+			totalSteps += deltaSteps;
+			totalPos += ((60 / curBPM) * 1000 / 4) * deltaSteps;
+		}
+		trace("new BPM map BUDDY " + bpmChangeMap);
+	}
+
+	public static function changeBPM(newBpm:Float)
+	{
+		bpm = newBpm;
+
+		crochet = ((60 / bpm) * 1000);
+		stepCrochet = crochet / 4;
+	}
+}
+
+class Rating
+{
+	public var name:String = '';
+	public var image:String = '';
+	public var counter:String = '';
+	public var hitWindow:Null<Int> = 0; //ms
+	public var ratingMod:Float = 1;
+	public var score:Int = 350;
+	public var noteSplash:Bool = true;
+
+	public function new(name:String)
+	{
+		this.name = name;
+		this.image = name;
+		this.counter = name + 's';
+		this.hitWindow = Reflect.field(ClientPrefs, name + 'Window');
+		if(hitWindow == null)
+		{
+			hitWindow = 0;
+		}
+	}
+
+	public function increase(blah:Int = 1)
+	{
+		Reflect.setField(PlayState.instance, counter, Reflect.field(PlayState.instance, counter) + blah);
+	}
+}
