c061c07623201ecb483d3a838c273290324c80f4
riconuts <aresgamer95@gmail.com>
2023-04-11 00:48:19 -0400

    Update MasterEditorMenu.hx

--- source/editors/MasterEditorMenu.hx
+++ source/editors/MasterEditorMenu.hx
@@ -45,7 +45,7 @@ class MasterEditorMenu extends MusicBeatState
 		DiscordClient.changePresence("Editors Main Menu", null);
 		#end
 
-		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuBGDesat'));
 		bg.scrollFactor.set();
 		bg.color = 0xFF353535;
 		add(bg);
