96b45c2810cf079a9891d0c297a83e9c6ba1d740
nebulazorua <rainbowsmokefox@gmail.com>
2023-07-14 11:27:58 +0800

    oops

--- source/hud/AdvancedHUD.hx
+++ source/hud/AdvancedHUD.hx
@@ -286,7 +286,7 @@ class AdvancedHUD extends BaseHUD
 				}
 
 				// dont need to check songHighRating != 0 because if songWifeHighscore isnt 0 then the rating prob isnt either
-				ratingTxt.color = !PlayState.instance.saveScore ? 0x818181 : ((songWifeHighscore != 0 && val > songWifeHighscore && val > songHighRating) ? 0xFFD800 : 0xFFFFFF);
+				ratingTxt.color = !PlayState.instance.saveScore ? 0x818181 : ((songWifeHighscore != 0 && val > songWifeHighscore && stats.ratingPercent > songHighRating) ? 0xFFD800 : 0xFFFFFF);
 			case 'grade':
 				FlxTween.cancelTweensOf(gradeTxt.scale);
 				gradeTxt.scale.set(1.2, 1.2);
