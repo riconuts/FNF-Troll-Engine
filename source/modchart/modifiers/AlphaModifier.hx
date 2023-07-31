92aac0d93807983446e660ef978f0cb0d0c22ee3
nebulazorua <rainbowsmokefox@gmail.com>
2023-07-31 08:04:45 +0800

    fixed thing

--- source/modchart/modifiers/AlphaModifier.hx
+++ source/modchart/modifiers/AlphaModifier.hx
@@ -36,7 +36,7 @@ class AlphaModifier extends NoteModifier {
   }
 
   function getVisibility(yPos:Float,player:Int,note:Note):Float{
-    var distFromCenter = yPos;
+    var distFromCenter = yPos - (FlxG.height * 0.5);
     var alpha:Float = 0;
 
     if(yPos<0 && getSubmodValue("stealthPastReceptors", player)==0)
@@ -65,7 +65,7 @@ class AlphaModifier extends NoteModifier {
     }
 
     if(getSubmodValue("vanish",player)!=0){
-      var realFadeDist:Float = 360;
+      var realFadeDist:Float = 120;
       alpha += CoolUtil.scale(Math.abs(distFromCenter),realFadeDist,2*realFadeDist,-1,0)*getSubmodValue("vanish",player);
     }
 
