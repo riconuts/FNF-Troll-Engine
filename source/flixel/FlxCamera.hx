e9e7309f54d38f407037bc90845266dbfc5b5339
riconuts <aresgamer95@gmail.com>
2023-08-20 01:16:52 -0400

    Update FlxCamera.hx

--- source/flixel/FlxCamera.hx
+++ source/flixel/FlxCamera.hx
@@ -498,9 +498,12 @@ class FlxCamera extends FlxBasic
 	var _point:FlxPoint = FlxPoint.get();
 
 	/**
-	 * Internal, the filters array to be applied to the camera.
+	 * The filters array to be applied to the camera.
 	 */
-	var _filters:Array<BitmapFilter>;
+	public var filters:Array<BitmapFilter>;
+
+	@:deprecated("_filters is deprecated, use filters instead")
+	var _filters(get, set):Null<Array<BitmapFilter>>;
 
 	/**
 	 * Camera's initial zoom value. Used for camera's scale handling.
@@ -1155,7 +1158,7 @@ class FlxCamera extends FlxBasic
 		updateFlash(elapsed);
 		updateFade(elapsed);
 
-		flashSprite.filters = (ClientPrefs.shaders != 'Off' && filtersEnabled) ? _filters : null;
+		flashSprite.filters = (ClientPrefs.shaders != 'Off' && filtersEnabled) ? filters : null;
 
 		updateFlashSpritePosition();
 		updateShake(elapsed);
@@ -1592,9 +1595,10 @@ class FlxCamera extends FlxBasic
 	/**
 	 * Sets the filter array to be applied to the camera.
 	 */
+	@:deprecated("setFilters() is deprecated, use the filters array instead")
 	public function setFilters(filters:Array<BitmapFilter>):Void
 	{
-		_filters = filters;
+		this.filters = filters;
 	}
 
 	/**
@@ -2148,7 +2152,16 @@ class FlxCamera extends FlxBasic
 	{
 		return viewMarginBottom;
 	}
-	
+
+	inline function get__filters():Array<BitmapFilter>
+	{
+		return filters;
+	}
+
+	inline function set__filters(Value:Array<BitmapFilter>):Array<BitmapFilter>
+	{
+		return filters = Value;
+	}	
 }
 
 enum FlxCameraFollowStyle
