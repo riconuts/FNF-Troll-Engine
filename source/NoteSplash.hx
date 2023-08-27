7813dac204d17e5d427f4f48e3cab87a2bf09cb1
riconuts <aresgamer95@gmail.com>
2023-08-27 02:10:14 -0400

    Update NoteSplash.hx

--- source/NoteSplash.hx
+++ source/NoteSplash.hx
@@ -29,13 +29,16 @@ class NoteSplash extends NoteObject
 		//antialiasing = ClientPrefs.globalAntialiasing;
 	}
 
-	public function setupNoteSplash(x:Float, y:Float, note:Int = 0, texture:String = null, hueColor:Float = 0, satColor:Float = 0, brtColor:Float = 0) {
+	public var animationAmount:Int = 2;
+	public function setupNoteSplash(x:Float, y:Float, note:Int = 0, texture:String = null, hueColor:Float = 0, satColor:Float = 0, brtColor:Float = 0) 
+	{
 		if (FlxG.state == PlayState.instance){
 			if (PlayState.instance.callOnHScripts("preSetupNoteSplash", [x, y, note, texture, hueColor, satColor, brtColor], ["this" => this, "noteData" => noteData]) == Globals.Function_Stop)
 				return;
-			
 		}
+
 		setPosition(x, y);
+		animationAmount = 2;
 		alpha = 0.6;
 		scale.set(0.8, 0.8);
 		updateHitbox();
@@ -52,34 +55,36 @@ class NoteSplash extends NoteObject
 			if(FlxG.state == PlayState.instance)
 				ret = PlayState.instance.callOnHScripts("loadSplashAnims", [texture], ["this" => this, "noteData" => noteData]);
 			
-			if (ret != Globals.Function_Stop)loadAnims(texture);
+			if (ret != Globals.Function_Stop) loadAnims(texture);
 			
 		}
 
-		
 		colorSwap.hue = hueColor;
 		colorSwap.saturation = satColor;
 		colorSwap.brightness = brtColor;
 
 		PlayState.instance.callOnHScripts("postSetupNoteSplash", [x, y, note, texture, hueColor, satColor, brtColor], ["this" => this, "noteData" => noteData]);
-		var animNum:Int = FlxG.random.int(1, 2);
-		animation.play('note' + note + '-' + animNum, true);
-		if(animation.curAnim != null)animation.curAnim.frameRate = 24 + FlxG.random.int(-2, 2);
+
+		animation.play('note$note-${FlxG.random.int(1, animationAmount)}', true);
+		if (animation.curAnim != null) 
+			animation.curAnim.frameRate = 24 + FlxG.random.int(-2, 2);
 	}
 
 	function loadAnims(skin:String) {
 		frames = Paths.getSparrowAtlas(skin);
-		for (i in 1...3)
+		for (i in 1...animationAmount+1)
 		{
+			animation.addByPrefix("note0-" + i, "note splash purple " + i, 24, false);
 			animation.addByPrefix("note1-" + i, "note splash blue " + i, 24, false);
 			animation.addByPrefix("note2-" + i, "note splash green " + i, 24, false);
-			animation.addByPrefix("note0-" + i, "note splash purple " + i, 24, false);
 			animation.addByPrefix("note3-" + i, "note splash red " + i, 24, false);
 		}
 	}
 
-	override function update(elapsed:Float) {
-		if(animation.curAnim != null)if(animation.curAnim.finished) kill();
+	override function update(elapsed:Float) 
+	{
+		if(animation.curAnim != null && animation.curAnim.finished) 
+			kill();
 
 		super.update(elapsed);
 	}
