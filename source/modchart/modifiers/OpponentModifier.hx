282c3ec7dad3387b69d35f5eff30315b4025de42
riconuts <aresgamer95@gmail.com>
2023-10-18 21:45:36 -0300

    opponentSwap now takes game width into consideration

--- source/modchart/modifiers/OpponentModifier.hx
+++ source/modchart/modifiers/OpponentModifier.hx
@@ -14,7 +14,7 @@ class OpponentModifier extends NoteModifier {
 
 	override function getPos(diff:Float, tDiff:Float, beat:Float, pos:Vector3, data:Int, player:Int, obj:FlxSprite, field:NoteField)
     {
-		var distX = 640 / (modMgr.playerAmount * 0.5); // theres probably a way to optimize this (division is expensive!) but i think this is a good way of doing this rn
+		var distX = FlxG.width / modMgr.playerAmount; // theres probably a way to optimize this (division is expensive!) but i think this is a good way of doing this rn
 
 		pos.x += distX * sign((player + 1) * 2 - 3) * getValue(player);
 		// any pN > 0 should go right whereas any pN < 0 should go left 
