c2afad8af676411d1d7ff385c06fa44795cadc93
nebulazorua <rainbowsmokefox@gmail.com>
2023-10-31 14:48:54 +0800

    z

--- source/shaders/ColorSwap.hx
+++ source/shaders/ColorSwap.hx
@@ -126,9 +126,43 @@ class ColorSwapShader extends FlxShader
 			return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
 		}
 
+        vec4 colorMult(vec4 color){
+            if (!hasTransform)
+			{
+				return color;
+			}
+
+			if (color.a == 0.0)
+			{
+				return vec4(0.0, 0.0, 0.0, 0.0);
+			}
+
+			if (!hasColorTransform)
+			{
+				return color * openfl_Alphav;
+			}
+
+			color = vec4(color.rgb / color.a, color.a);
+
+			mat4 colorMultiplier = mat4(0);
+			colorMultiplier[0][0] = openfl_ColorMultiplierv.x;
+			colorMultiplier[1][1] = openfl_ColorMultiplierv.y;
+			colorMultiplier[2][2] = openfl_ColorMultiplierv.z;
+			colorMultiplier[3][3] = openfl_ColorMultiplierv.w;
+
+			color = clamp(openfl_ColorOffsetv + (color * colorMultiplier), 0.0, 1.0);
+
+			if (color.a > 0.0)
+			{
+				return vec4(color.rgb * color.a * openfl_Alphav, color.a * openfl_Alphav);
+			}
+
+            return vec4(0.0, 0.0, 0.0, 0.0);
+        }
+
 		void main()
 		{
-			vec4 color = flixel_texture2D(bitmap, openfl_TextureCoordv);
+			vec4 color = texture2D(bitmap, openfl_TextureCoordv);
 
 			vec4 swagColor = vec4(rgb2hsv(vec3(color[0], color[1], color[2])), color[3]);
 
@@ -149,10 +183,10 @@ class ColorSwapShader extends FlxShader
 			color = vec4(hsv2rgb(vec3(swagColor[0], swagColor[1], swagColor[2])), swagColor[3]);
 
 			if(flash != 0.0){
-				color = mix(color,flashColor,flash) * color.a;
+				color = mix(color, flashColor, flash) * color.a;
 			}
 			color *= daAlpha;
-			gl_FragColor = color;
+			gl_FragColor = colorMult(color);
 
 		}')
 	public function new()
