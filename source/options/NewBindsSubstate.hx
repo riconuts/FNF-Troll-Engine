45e2132b1bf0831431d3513675e372835b6c823f
riconuts <aresgamer95@gmail.com>
2024-02-20 04:14:11 -0300

    indentation

--- source/options/NewBindsSubstate.hx
+++ source/options/NewBindsSubstate.hx
@@ -48,18 +48,18 @@ class NewBindsSubstate extends MusicBeatSubstate  {
 
 	public var changedBind:(String, Int, FlxKey) -> Void;
 
-    // anything beyond this point prob shouldnt be touched too much
-    var cam:FlxCamera = new FlxCamera();
+	// anything beyond this point prob shouldnt be touched too much
+	var cam:FlxCamera = new FlxCamera();
 	var overCam:FlxCamera = new FlxCamera();
-    var scrollableCam:FlxCamera = new FlxCamera();
+	var scrollableCam:FlxCamera = new FlxCamera();
 
 	var camFollow = new FlxPoint(0, 0);
 	var camFollowPos = new FlxObject(0, 0);
 
-    var bindIndex:Int = -1;
-    var bindID:Int = 0;
-    var bindButtons:Array<Array<BindButton>> = []; // the actual bind buttons. used for input etc lol
-    var internals:Array<String> = [];
+	var bindIndex:Int = -1;
+	var bindID:Int = 0;
+	var bindButtons:Array<Array<BindButton>> = []; // the actual bind buttons. used for input etc lol
+	var internals:Array<String> = [];
 	var resetBinds:FlxUI9SliceSprite;
 
 	@:noCompletion
@@ -81,17 +81,17 @@ class NewBindsSubstate extends MusicBeatSubstate  {
 		return false;
 	}
 
-    var height:Float = 0;
+	var height:Float = 0;
 
-    var popupTitle:FlxText;
-    var popupText:FlxText;
-    var unbindText:FlxText;
+	var popupTitle:FlxText;
+	var popupText:FlxText;
+	var unbindText:FlxText;
 
 	override public function create()
 	{
 		super.create();
-        
-        FlxG.cameras.add(cam, false);
+		
+		FlxG.cameras.add(cam, false);
 		FlxG.cameras.add(scrollableCam, false);
 		FlxG.cameras.add(overCam, false);
 		
@@ -127,10 +127,10 @@ class NewBindsSubstate extends MusicBeatSubstate  {
 		popupText.cameras = [overCam];
 		popupTitle.cameras = [overCam];
 
-        add(popupDrop);
-        add(popupTitle);
-        add(popupText);
-        add(unbindText);
+		add(popupDrop);
+		add(popupTitle);
+		add(popupText);
+		add(unbindText);
 
 		var optionMenu = new FlxSprite(84, 80, CoolUtil.makeOutlinedGraphic(920, 570, FlxColor.fromRGB(82, 82, 82), 2, FlxColor.fromRGB(70, 70, 70)));
 		optionMenu.alpha = 0.8;
@@ -150,18 +150,18 @@ class NewBindsSubstate extends MusicBeatSubstate  {
 		scrollableCam.follow(camFollowPos);
 		var group = new FlxTypedGroup<FlxObject>();
 		
-        
-        var daY:Float = 0;
-        var idx:Int = 0;
-        for(data in binds){
-            var label = data[0];
+		
+		var daY:Float = 0;
+		var idx:Int = 0;
+		for(data in binds){
+			var label = data[0];
 
-            if(data.length > 1){
-                // its a bind
+			if(data.length > 1){
+				// its a bind
 				var internal = data[1];
-                var buttArray:Array<BindButton> = [];
+				var buttArray:Array<BindButton> = [];
 				internals.push(data[1]);
-                
+				
 				var text = new FlxText(16, daY, 0, label, 16);
 				text.cameras = [scrollableCam];
 				text.setFormat(Paths.font("calibri.ttf"), 28, 0xFFFFFFFF, FlxTextAlign.LEFT);
@@ -191,24 +191,24 @@ class NewBindsSubstate extends MusicBeatSubstate  {
 				secButt.cameras = [scrollableCam];
 				buttArray.push(secButt);
 
-                group.add(drop);
-                group.add(text);
+				group.add(drop);
+				group.add(text);
 				group.add(prButt);
-                group.add(secButt);
+				group.add(secButt);
 				daY += height + 3;
 
 				bindButtons.push(buttArray);
-            }else{
-                // its just a label
+			}else{
+				// its just a label
 				var text = new FlxText(8, daY, 0, label, 16);
 				text.cameras = [scrollableCam];
 				text.setFormat(Paths.font("calibrib.ttf"), 32, 0xFFFFFFFF, FlxTextAlign.LEFT);
 				group.add(text);
 				daY += text.height;
-            }
+			}
 			idx++;
-        }
-        
+		}
+		
 
 		var text = new FlxText(16, daY, 0, Paths.getString("control_default"), 16);
 		text.cameras = [scrollableCam];
@@ -228,18 +228,18 @@ class NewBindsSubstate extends MusicBeatSubstate  {
 
 		this.height = daY > scrollableCam.height ? daY - scrollableCam.height : 0;
 
-        group.add(resetBinds);
+		group.add(resetBinds);
 		group.add(text);
-        add(group);
-    }
+		add(group);
+	}
 
-    override function update(elapsed:Float){
-        super.update(elapsed);
+	override function update(elapsed:Float){
+		super.update(elapsed);
 
 		var lerpVal = (elapsed / (1 / 60));
 		cam.bgColor = FlxColor.interpolate(cam.bgColor, 0x80000000, lerpVal * 0.1);
 
-        if(bindIndex == -1){
+		if(bindIndex == -1){
 			if (controls.BACK)
 			{
 				close();
@@ -249,74 +249,74 @@ class NewBindsSubstate extends MusicBeatSubstate  {
 				FlxG.sound.play(Paths.sound('cancelMenu'));
 				return;
 			}
-            if(FlxG.mouse.justPressed){
-                for (index in 0...bindButtons.length){
+			if(FlxG.mouse.justPressed){
+				for (index in 0...bindButtons.length){
 					var fuck = bindButtons[index];
-                    for(id => butt in fuck){
-                        if(overlaps(butt, scrollableCam)){
+					for(id => butt in fuck){
+						if(overlaps(butt, scrollableCam)){
 							var internal = internals[index];
 							bindID = id;
-                            bindIndex = index;
+							bindIndex = index;
 
 							unbindText.visible = forcedBind.contains(internal);
 							popupText.text = Paths.getString("control_rebind").replace("{cancelKey}", "[Backspace]"); //'Press any key to bind, or press [BACKSPACE] to cancel.';
 							if (ClientPrefs.keyBinds.get(internal)[id]!=NONE)
 								popupText.text += "\n" + Paths.getString("control_unbind").replace("{unbindKey}", '[${InputFormatter.getKeyName(ClientPrefs.keyBinds.get(internal)[id])}]');
-                            //'\nPress [${InputFormatter.getKeyName(ClientPrefs.keyBinds.get(internal)[id])}] to unbind.';
+							//'\nPress [${InputFormatter.getKeyName(ClientPrefs.keyBinds.get(internal)[id])}] to unbind.';
 
 							popupTitle.text = Paths.getString("control_binding").replace("{controlNameUpper}", binds[butt.ID][0].toUpperCase()).replace("{controlName}", binds[butt.ID][0]); //"CURRENTLY BINDING " + binds[butt.ID][0].toUpperCase();
-                        }
-                    }
-                }
+						}
+					}
+				}
 
-                if(bindIndex == -1){
+				if(bindIndex == -1){
 					if (overlaps(resetBinds, scrollableCam)){
-                        // i hate haxeflixel lmao
+						// i hate haxeflixel lmao
 						for (key => val in ClientPrefs.defaultKeys.copy()){
 							ClientPrefs.keyBinds.set(key, []);
-                            for(i => v in val){
+							for(i => v in val){
 								if (changedBind!=null)
 									changedBind(key, i, v);
-                                ClientPrefs.keyBinds.get(key)[i] = v;
-                            }
-                        }
+								ClientPrefs.keyBinds.get(key)[i] = v;
+							}
+						}
 						
-                        for(index => fuck in bindButtons){
-                            for(id => butt in fuck){
+						for(index => fuck in bindButtons){
+							for(id => butt in fuck){
 								var internal = internals[index];
 								butt.bind = ClientPrefs.keyBinds.get(internal)[id];
-                            }
-                        }
+							}
+						}
 						FlxG.sound.play(Paths.sound('confirmMenu'));
 						ClientPrefs.saveBinds();
 						ClientPrefs.reloadControls();
-                    }
-                }
-            }
-            var movement:Float = -FlxG.mouse.wheel * 45;
-            var es:Float = elapsed / (1 / 60);
+					}
+				}
+			}
+			var movement:Float = -FlxG.mouse.wheel * 45;
+			var es:Float = elapsed / (1 / 60);
 
-            if (FlxG.keys.pressed.PAGEUP)
-                movement -= 25 * es;
-            if (FlxG.keys.pressed.PAGEDOWN)
-                movement += 25 * es;
+			if (FlxG.keys.pressed.PAGEUP)
+				movement -= 25 * es;
+			if (FlxG.keys.pressed.PAGEDOWN)
+				movement += 25 * es;
 
-            camFollow.y += movement;
-            camFollowPos.y += movement;
-            if (camFollow.y < 0)
-                camFollow.y = 0;
-            if (camFollow.y > height)
-                camFollow.y = height; 
+			camFollow.y += movement;
+			camFollowPos.y += movement;
+			if (camFollow.y < 0)
+				camFollow.y = 0;
+			if (camFollow.y > height)
+				camFollow.y = height; 
 
-            camFollowPos.setPosition(FlxMath.lerp(camFollowPos.x, camFollow.x, lerpVal), FlxMath.lerp(camFollowPos.y, camFollow.y, lerpVal));
-            if (camFollowPos.y < 0)
-                camFollowPos.y = 0;
+			camFollowPos.setPosition(FlxMath.lerp(camFollowPos.x, camFollow.x, lerpVal), FlxMath.lerp(camFollowPos.y, camFollow.y, lerpVal));
+			if (camFollowPos.y < 0)
+				camFollowPos.y = 0;
 
-            if (camFollowPos.y > height)
-                camFollowPos.y = height; 
+			if (camFollowPos.y > height)
+				camFollowPos.y = height; 
 
 			overCam.alpha = FlxMath.lerp(overCam.alpha, 0, lerpVal * 0.2);
-        }else{
+		}else{
 			overCam.alpha = FlxMath.lerp(overCam.alpha, 1, lerpVal * 0.2);
 
 			var keyPressed:FlxKey = FlxG.keys.firstJustPressed();
@@ -326,72 +326,72 @@ class NewBindsSubstate extends MusicBeatSubstate  {
 				bindIndex = -1;
 				FlxG.sound.play(Paths.sound('cancelMenu'));
 			}else if (keyPressed != NONE)
-            {
+			{
 				FlxG.sound.play(Paths.sound('confirmMenu'));
-                var opp = bindID == 0 ? 1 : 0 ;
-                var internal = internals[bindIndex];
+				var opp = bindID == 0 ? 1 : 0 ;
+				var internal = internals[bindIndex];
 				trace("bound " + internal + " (" + bindID + ") to " + InputFormatter.getKeyName(keyPressed));
 				var binds = ClientPrefs.keyBinds.get(internal);
 				if (binds[bindID] == keyPressed)
 					keyPressed = NONE;
-                else if(binds[opp] == keyPressed){
+				else if(binds[opp] == keyPressed){
 					if (changedBind != null)
 						changedBind(internal, opp, NONE);
-                    binds[opp] = NONE;
+					binds[opp] = NONE;
 					bindButtons[bindIndex][opp].bind = NONE;
-                }
+				}
 
-                if(forcedBind.contains(internal)){
-                    if(keyPressed == NONE && binds[opp] == NONE){
+				if(forcedBind.contains(internal)){
+					if(keyPressed == NONE && binds[opp] == NONE){
 						var defaults = ClientPrefs.defaultKeys.get(internal);
-                        // atleast ONE needs to be bound, so use a default
+						// atleast ONE needs to be bound, so use a default
 						if (defaults[bindID] == NONE)
-                            keyPressed = defaults[opp];
-                        else
-						    keyPressed = defaults[bindID]; 
-                    }
-                }
+							keyPressed = defaults[opp];
+						else
+							keyPressed = defaults[bindID]; 
+					}
+				}
 				if (changedBind != null)
 					changedBind(internal, bindID, keyPressed);
 				binds[bindID] = keyPressed;
 
-                bindButtons[bindIndex][bindID].bind = keyPressed;
+				bindButtons[bindIndex][bindID].bind = keyPressed;
 				ClientPrefs.keyBinds.set(internal, binds);
 				ClientPrefs.saveBinds();
-                ClientPrefs.reloadControls();
+				ClientPrefs.reloadControls();
 
-                bindID = 0;
+				bindID = 0;
 				bindIndex = -1;
-            }
-        }
+			}
+		}
 
-    }
+	}
 }
 
 class BindButton extends FlxUI9SliceSprite
 {
-    public var textObject:FlxText;
-    public var bind(default, set):FlxKey = NONE;
+	public var textObject:FlxText;
+	public var bind(default, set):FlxKey = NONE;
 
-    function set_bind(key:FlxKey){
-        textObject.text = InputFormatter.getKeyName(key);
-        return bind = key;
-    }
+	function set_bind(key:FlxKey){
+		textObject.text = InputFormatter.getKeyName(key);
+		return bind = key;
+	}
 
-    public function new(?x:Float, ?y:Float, ?rect:Rectangle, bind:FlxKey = NONE){
+	public function new(?x:Float, ?y:Float, ?rect:Rectangle, bind:FlxKey = NONE){
 		super(x, y, Paths.image("optionsMenu/backdrop"), rect, [22, 22, 89, 89]);
 
 		textObject = new FlxText(x, y, 0, InputFormatter.getKeyName(bind), 16);
 		textObject.setFormat(Paths.font("calibri.ttf"), 24, 0xFFFFFFFF, FlxTextAlign.CENTER);
 		textObject.updateHitbox();
 		textObject.y += (height - textObject.height) / 2;
-        this.bind = bind;
-    }
+		this.bind = bind;
+	}
 
-    override function draw(){
-        super.draw();
+	override function draw(){
+		super.draw();
 		textObject.draw();
-    }
+	}
 
 	override function kill()
 	{
@@ -413,8 +413,8 @@ class BindButton extends FlxUI9SliceSprite
 
 	override function set_active(val:Bool)
 	{
-        textObject.active = val;
-        return active = val;
+		textObject.active = val;
+		return active = val;
 	}
 
 	override function set_visible(val:Bool)
@@ -432,21 +432,21 @@ class BindButton extends FlxUI9SliceSprite
 	override function set_x(val:Float)
 	{
 		if (textObject!=null)
-		    textObject.x += val - x;
+			textObject.x += val - x;
 		return x = val;
 	}
 
 	override function set_y(val:Float)
 	{
 		if (textObject != null)
-		    textObject.y += val - y;
+			textObject.y += val - y;
 
 		return y = val;
 	}
 
-    override function update(elapsed:Float){
+	override function update(elapsed:Float){
 		textObject.fieldWidth = width;
-        super.update(elapsed);
+		super.update(elapsed);
 		textObject.update(elapsed);
-    }
+	}
 }
