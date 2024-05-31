b354e32359d5c2e14d7e0480f9232dccfce5cc0a
riconuts <aresgamer95@gmail.com>
2024-05-31 11:46:07 -0400

    attempt to make FlxSoundTray easier to extend
    flixel please

--- source/FNFGame.hx
+++ source/FNFGame.hx
@@ -5,28 +5,38 @@ import scripts.FunkinHScript;
 import scripts.FunkinHScript.HScriptState;
 #end
 
-class FNFGame extends FlxGame {
+class FNFGame extends FlxGame
+{
+	public function new(gameWidth = 0, gameHeight = 0, ?initialState:Class<FlxState>, updateFramerate = 60, drawFramerate = 60, skipSplash = false, startFullscreen = false)
+	{
+		super(gameWidth, gameHeight, initialState, updateFramerate, drawFramerate, skipSplash, startFullscreen);
+		_customSoundTray = flixel.system.ui.DefaultFlxSoundTray;
+	}
+
 	#if SCRIPTABLE_STATES
 	override function switchState():Void
 	{
-		if (_requestedState is MusicBeatState){
+		if (_requestedState is MusicBeatState)
+		{
 			var state:MusicBeatState = cast _requestedState;
-            if (state.canBeScripted){
-                var className = Type.getClassName(Type.getClass(_requestedState));
-                for (filePath in Paths.getFolders("states"))
-                {
+			if (state.canBeScripted)
+			{
+				var className = Type.getClassName(Type.getClass(_requestedState));
+				for (filePath in Paths.getFolders("states"))
+				{
 					var fileName = 'override/$className.hscript';
-					if (Paths.exists(filePath + fileName)){
-                        _requestedState.destroy();
-                        _requestedState = new HScriptState(fileName);
+					if (Paths.exists(filePath + fileName))
+					{
+						_requestedState.destroy();
+						_requestedState = new HScriptState(fileName);
 						trace(fileName);
-                        return super.switchState();
-                    }
-                }
-            }
-        } 
+						return super.switchState();
+					}
+				}
+			}
+		}
 
-        return super.switchState();
-    }
+		return super.switchState();
+	}
 	#end
 }
--- source/flixel/system/ui/FlxSoundTray.hx
+++ source/flixel/system/ui/FlxSoundTray.hx
@@ -22,27 +22,8 @@ import flash.text.GridFitType;
  */
 class FlxSoundTray extends Sprite
 {
-	/**
-	 * Because reading any data from DisplayObject is insanely expensive in hxcpp, keep track of whether we need to update it or not.
-	 */
+	/** Whether the sound tray should be updated or not. **/
 	public var active:Bool;
-
-	/**
-	 * Helps us auto-hide the sound tray after a volume change.
-	 */
-	var _timer:Float;
-
-	/**
-	 * Helps display the volume bars on the sound tray.
-	 */
-	var _bars:Array<Bitmap>;
-
-	/**
-	 * How wide the sound tray background is.
-	 */
-	var _width:Int = 80;
-
-	var _defaultScale:Float = 2.0;
 
 	/**The sound used when increasing the volume.**/
 	public var volumeUpSound:String = "flixel/sounds/beep";
@@ -54,92 +35,11 @@ class FlxSoundTray extends Sprite
 	public var silent:Bool = false;
 
 	/**
-	 * Sets up the "sound tray", the little volume meter that pops down sometimes.
-	 */
-	@:keep
-	public function new()
-	{
-		super();
-
-		visible = false;
-		scaleX = _defaultScale;
-		scaleY = _defaultScale;
-		var tmp:Bitmap = new Bitmap(new BitmapData(_width, 30, true, 0x7F000000));
-		screenCenter();
-		addChild(tmp);
-
-		var text:TextField = new TextField();
-		text.width = tmp.width;
-		text.height = tmp.height;
-		text.multiline = true;
-		text.wordWrap = true;
-		text.selectable = false;
-
-		#if flash
-		text.embedFonts = true;
-		text.antiAliasType = AntiAliasType.NORMAL;
-		text.gridFitType = GridFitType.PIXEL;
-		#else
-		#end
-		
-		#if tgt
-		var dtf:TextFormat = new TextFormat(Paths.font("calibrib.ttf"), 10, 0xffffff);
-		#else
-		var dtf:TextFormat = new TextFormat(FlxAssets.FONT_DEFAULT, 8, 0xffffff);
-		#end
-		dtf.align = TextFormatAlign.CENTER;
-		text.defaultTextFormat = dtf;
-		addChild(text);
-		text.text = "VOLUME";
-		text.y = 16;
-
-		var bx:Int = 10;
-		var by:Int = 14;
-		_bars = new Array();
-
-		for (i in 0...10)
-		{
-			tmp = new Bitmap(new BitmapData(4, i + 1, false, FlxColor.WHITE));
-			tmp.x = bx;
-			tmp.y = by;
-			addChild(tmp);
-			_bars.push(tmp);
-			bx += 6;
-			by--;
-		}
-
-		y = -height;
-		visible = false;
-	}
-
-	/**
-	 * This function just updates the soundtray object.
+	 * This function just updates the sound tray object.
 	 */
 	public function update(MS:Float):Void
 	{
-		// Animate stupid sound tray thing
-		if (_timer > 0)
-		{
-			_timer -= MS / 1000;
-		}
-		else if (y > -height)
-		{
-			y -= (MS / 1000) * FlxG.height * 2;
-
-			if (y <= -height)
-			{
-				visible = false;
-				active = false;
-
-				// Save sound preferences
-				if (FlxG.save.isBound)
-				{
-					FlxG.save.data.mute = FlxG.sound.muted;
-					FlxG.save.data.volume = FlxG.sound.volume;
-					FlxG.save.flush();
-				}
-			}
-		}
+		
 	}
 
 	/**
@@ -149,43 +49,34 @@ class FlxSoundTray extends Sprite
 	 */
 	public function show(up:Bool = false):Void
 	{
+		playVolumeSound(up);
+		saveVolumeData();
+	}
+
+	public function playVolumeSound(up:Bool = false)
+	{
 		if (!silent)
 		{
 			var sound = FlxAssets.getSound(up ? volumeUpSound : volumeDownSound);
 			if (sound != null)
 				FlxG.sound.load(sound).play();
 		}
+	}
 
-		_timer = 1;
-		y = 0;
-		visible = true;
-		active = true;
-		var globalVolume:Int = Math.round(FlxG.sound.volume * 10);
-
-		if (FlxG.sound.muted)
+	/** Save sound preferences **/
+	public function saveVolumeData()
+	{
+		if (FlxG.save.isBound)
 		{
-			globalVolume = 0;
-		}
-
-		for (i in 0..._bars.length)
-		{
-			if (i < globalVolume)
-			{
-				_bars[i].alpha = 1;
-			}
-			else
-			{
-				_bars[i].alpha = 0.5;
-			}
+			FlxG.save.data.mute = FlxG.sound.muted;
+			FlxG.save.data.volume = FlxG.sound.volume;
+			FlxG.save.flush();
 		}
 	}
 
 	public function screenCenter():Void
 	{
-		scaleX = _defaultScale;
-		scaleY = _defaultScale;
 
-		x = (0.5 * (Lib.current.stage.stageWidth - _width * _defaultScale) - FlxG.game.x);
 	}
 }
 #end
