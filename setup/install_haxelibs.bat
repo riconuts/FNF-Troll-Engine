381be4eeef9b3799adf3f8eb8c7ec78e7b633fa5
riconuts <aresgamer95@gmail.com>
2024-06-13 17:36:52 -0400

    forgot

--- setup/install_haxelibs.bat
+++ setup/install_haxelibs.bat
@@ -6,3 +6,4 @@ haxelib --always --quiet install flixel-addons 3.1.1
 haxelib --always --quiet install hscript 2.5.0
 haxelib --always --quiet install hxvlc 1.6.1
 haxelib --always --quiet git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib --always --quiet git linc_luajit https://github.com/nebulazorua/linc_luajit
