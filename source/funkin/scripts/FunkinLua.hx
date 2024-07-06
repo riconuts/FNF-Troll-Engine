2748e9ceafdaf80d1d94659bffc35cd59d72dca9
riconuts <aresgamer95@gmail.com>
2024-07-06 19:22:37 -0400

    Update FunkinLua.hx

--- source/funkin/scripts/FunkinLua.hx
+++ source/funkin/scripts/FunkinLua.hx
@@ -68,9 +68,10 @@ class FunkinLua extends FunkinScript
 
 		// Song/Week shit
 		setDefaultVars();
-        for(key => val in vars)
-            set(key, val);
-        
+		if (vars != null){
+			for(key => val in vars)
+				set(key, val);
+		}
 
 		// Camera poo
 		set('cameraX', 0);
@@ -91,7 +92,7 @@ class FunkinLua extends FunkinScript
 
 		set("curSection", 0);
 
-		set("playbackRate", 1.0); // so psych scripts give one error less 
+		set("playbackRate", 1.0); // for compatibility, do not give it the actual value.
 
 		for (i in 0...5) {
 			// annoying since some scripts use defaultPlayerStrumX/Y 4
