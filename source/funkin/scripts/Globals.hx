f26c4514aa2fe298220ae0af41d449d27d332319
riconuts <aresgamer95@gmail.com>
2024-09-03 14:16:49 -0400

    Update Globals.hx

--- source/funkin/scripts/Globals.hx
+++ source/funkin/scripts/Globals.hx
@@ -5,11 +5,11 @@ import funkin.states.GameOverSubstate;
 
 class Globals
 {
-	public static var Function_Stop:String = 'FUNC_STOP';
-	public static var Function_Continue:String = 'FUNC_CONT'; // i take back what i said
-	public static var Function_Halt:String = 'FUNC_HALT';
+	public static final Function_Stop:String = 'FUNC_STOP';
+	public static final Function_Continue:String = 'FUNC_CONT'; // i take back what i said
+	public static final Function_Halt:String = 'FUNC_HALT';
 
-	public static var variables:Map<String, Dynamic> = new Map(); // it MAKES WAY MORE SENSE FOR THIS TO BE HERE THAN IN PLAYSTATE GRRR BARK BARK
+	public static final variables:Map<String, Dynamic> = new Map(); // it MAKES WAY MORE SENSE FOR THIS TO BE HERE THAN IN PLAYSTATE GRRR BARK BARK
 
 	public static inline function getInstance()
 	{
