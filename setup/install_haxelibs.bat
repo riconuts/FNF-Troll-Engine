019843b7d7af8c010f63a1778e9c137138de461b
riconuts <aresgamer95@gmail.com>
2024-09-17 16:22:20 -0300

    use hxdiscord_rpc 1.1.1
    never pushed this one sorry. that version doesn't seem to work and i've been using this one myself

--- setup/install_haxelibs.bat
+++ setup/install_haxelibs.bat
@@ -5,6 +5,6 @@ haxelib --always --quiet install flixel-ui 2.6.1
 haxelib --always --quiet install flixel-addons 3.2.3
 haxelib --always --quiet git hscript https://github.com/nebulazorua/t-hscript
 haxelib --always --quiet install hxvlc 1.6.1
-haxelib --always --quiet git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc 3538a1c2bb07b04208cd014220207f8173acdb21
+haxelib --always --quiet install hxdiscord_rpc 1.1.1
 haxelib --always --quiet git linc_luajit https://github.com/riconuts/linc_luajit
 haxelib --always --quiet git moonchart https://github.com/MaybeMaru/moonchart
