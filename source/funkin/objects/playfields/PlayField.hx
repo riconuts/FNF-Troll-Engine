81fec26173ae0556289c0d95eb4c097420bfe632
nebulazorua <12458328+nebulazorua@users.noreply.github.com>
2024-10-11 14:13:07 +0800

    fixed an issue with receptor confirm being fucked on Monster
    resolves #49

--- source/funkin/objects/playfields/PlayField.hx
+++ source/funkin/objects/playfields/PlayField.hx
@@ -487,8 +487,9 @@ class PlayField extends FlxTypedGroup<FlxBasic>
 
                         
 						if(isHeld && !daNote.isRoll){
-							if (receptor.animation.finished || receptor.animation.curAnim.name != "confirm") 
-								receptor.playAnim("confirm", true);
+							if(daNote.unhitTail.length > 0)
+								if (receptor.animation.finished || receptor.animation.curAnim.name != "confirm") 
+									receptor.playAnim("confirm", true, daNote);
 							
 							daNote.tripProgress = 1.0;
 						}else
@@ -513,7 +514,7 @@ class PlayField extends FlxTypedGroup<FlxBasic>
 						}else{
 							for (tail in daNote.unhitTail)
 							{
-								if ((tail.strumTime - 25) <= Conductor.songPosition && !tail.wasGoodHit && !tail.tooLate){
+								if (tail.strumTime <= Conductor.songPosition && !tail.wasGoodHit && !tail.tooLate){
 									noteHitCallback(tail, this);
 								}
 							}
@@ -545,7 +546,7 @@ class PlayField extends FlxTypedGroup<FlxBasic>
 				} 
 
 				if((
-					(daNote.holdingTime>=daNote.sustainLength ) && daNote.sustainLength>0 ||
+					(daNote.holdingTime>=daNote.sustainLength && daNote.unhitTail.length == 0) && daNote.sustainLength>0 ||
 					daNote.isSustainNote && daNote.strumTime - Conductor.songPosition < -350 ||
 					!daNote.isSustainNote
 					&& (daNote.sustainLength == 0 || daNote.tooLate)
