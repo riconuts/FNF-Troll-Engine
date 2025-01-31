037425e0262103035910c8fa34b9ef097405c908
riconuts <riconuts0@gmail.com>
2025-01-30 22:09:05 -0300

    remove openfl_legacy

--- source/flixel/FlxSprite.hx
+++ source/flixel/FlxSprite.hx
@@ -288,9 +288,6 @@ class FlxSprite extends FlxObject
 	 * Avoid changing it frequently as this is a costly operation.
 	 * @since 4.1.0
 	 */
-	#if openfl_legacy
-	@:noCompletion
-	#end
 	public var shader:FlxShader;
 
 	/**
