199f5159e3257958500a8ce26b7d7c112516874c
nebulazorua <12458328+nebulazorua@users.noreply.github.com>
2025-03-04 22:03:06 +0800

    forgor

--- source/funkin/modchart/modifiers/optional/WobbleModifier.hx
+++ source/funkin/modchart/modifiers/optional/WobbleModifier.hx
@@ -11,5 +11,6 @@ class WobbleModifier extends NoteModifier {
 	override function getPos(visualDiff:Float, timeDiff:Float, beat:Float, pos:Vector3, column:Int, player:Int, obj:FlxSprite, field:NoteField) {
 		var val = getValue(player);
 		pos.x += FlxMath.fastSin((visualDiff / FlxG.height) * Math.PI * 3) * (val * 250);
+		return pos;
 	}
 }
