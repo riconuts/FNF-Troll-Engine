95ead288e76a7fa137ecb568b7cfd0e7f2e50e15
nebulazorua <12458328+nebulazorua@users.noreply.github.com>
2025-03-04 22:12:54 +0800

    wobbleholds to replicate shit like the mic'd up wiggleeffect thing

--- source/funkin/modchart/modifiers/optional/WobbleModifier.hx
+++ source/funkin/modchart/modifiers/optional/WobbleModifier.hx
@@ -4,13 +4,24 @@ package funkin.modchart.modifiers.optional;
 @:keep
 
 // Bopeebo Rumble "wobble"
+// Resembles putting WiggleEffect on the holds + notes
+
 class WobbleModifier extends NoteModifier {
 	override function getName()
 		return "wobble";
 		
-	override function getPos(visualDiff:Float, timeDiff:Float, beat:Float, pos:Vector3, column:Int, player:Int, obj:FlxSprite, field:NoteField) {
+	override function getPos(visualDiff:Float, timeDiff:Float, beat:Float, pos:Vector3, column:Int, player:Int, obj:NoteObject, field:NoteField) {
 		var val = getValue(player);
+		if(obj.objType == NOTE){
+			var note:Note = cast obj;
+			if (note.isSustainNote)
+				val += getSubmodValue("wobbleHolds", player);
+		}
 		pos.x += FlxMath.fastSin((visualDiff / FlxG.height) * Math.PI * 3) * (val * 250);
 		return pos;
 	}
+
+	override function getSubmods()
+		return ["wobbleHolds"];
+	
 }
