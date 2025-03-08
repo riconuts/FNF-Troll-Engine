1c578887a5a0773196e9389900354784c6a7038b
nebulazorua <12458328+nebulazorua@users.noreply.github.com>
2025-03-09 04:11:12 +0800

    riconuts

--- source/funkin/states/PlayState.hx
+++ source/funkin/states/PlayState.hx
@@ -1856,13 +1856,16 @@ class PlayState extends MusicBeatState
 		generatedMusic = true;
 	}
 
-	public function generateNotes(noteData:Array<SwagSection>, callScripts:Bool = true, addToFields:Bool = true, ?playfields:Array<PlayField>, ?notes:Array<Note>){
+	public function generateNotes(noteData:Array<SwagSection>, callScripts:Bool = true, addToFields:Bool = true, ?keyCount:Int, ?playfields:Array<PlayField>, ?notes:Array<Note>){
 
 		if (playfields == null)
 			playfields = this.playfields.members;
 
 		if (notes==null)
 			notes = this.allNotes;
+
+		if(keyCount == null)
+			keyCount = PlayState.keyCount;
 		
 		for (section in noteData) {
 			for (songNotes in section.sectionNotes) {
