5ed5041ed5e952e5eb313802e8d7aede77def0dd
riconuts <riconuts0@gmail.com>
2025-03-25 13:49:35 -0300

    flixel version 5.9.0
    it compiles, right? so like why not use 5.9.0

--- setup/windows-haxelibs.bat
+++ setup/windows-haxelibs.bat
@@ -7,7 +7,7 @@ cls
 @echo on
 haxelib --always --quiet install lime 8.1.3
 haxelib --always --quiet install openfl 9.3.4
-haxelib --always --quiet install flixel 5.6.2
+haxelib --always --quiet install flixel 5.9.0
 haxelib --always --quiet install flixel-ui 2.6.1
 haxelib --always --quiet install flixel-addons 3.2.3
 haxelib --always --quiet git hscript https://github.com/nebulazorua/t-hscript
