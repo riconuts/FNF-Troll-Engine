a772f9e694f465992f84103a6106f57e8afeca0e
riconuts <riconuts0@gmail.com>
2025-05-01 18:28:46 -0400

    Create Level.hx

---
+++ source/funkin/data/Level.hx
@@ -0,0 +1,208 @@
+package funkin.data;
+
+import funkin.data.Song;
+import funkin.states.StoryModeState;
+import funkin.scripts.FunkinHScript;
+import funkin.scripts.Globals;
+import flixel.group.FlxSpriteGroup;
+import flixel.util.typeLimit.OneOfTwo;
+import flixel.util.FlxColor;
+import haxe.io.Path;
+import haxe.Json;
+
+using funkin.CoolerStringTools;
+using StringTools;
+
+class Level {
+	var script:FunkinHScript;
+
+	public static function fromFile(fileName:String, ?index:Int = 0, ?id:String){
+		var json:Null<JSONLevelData> = Paths.exists(fileName + ".json") ? Json.parse(Paths.getContent(fileName + ".json")) : null;
+
+
+/* 		var scriptFile = Paths.getHScriptPath(fileName);
+		if (hscriptFile != null) {
+			var script = FunkinHScript.fromFile(hscriptFile, hscriptFile, defaultVars);
+			pushScript(script);
+			return this;
+		} */
+
+		var level:Level = new Level();
+		level.bgColor = CoolUtil.colorFromString(json?.bgColor ?? "#F9CF51");
+		
+		level.id = id ?? json?.id ?? Path.withoutDirectory(fileName);
+		level.index = json?.index ?? index;
+		level.name = json?.name ?? "NAME DOESNT EXIST IDIOT";
+		level.asset = json?.asset ?? "storymenu/titles/week1";
+		level.difficulties = json?.difficulties ?? level.difficulties;
+		level.props = json?.props ?? level.props;
+		level.songs = json?.songs ?? ["Test"];
+
+		for (ext in Paths.HSCRIPT_EXTENSIONS) {
+			var scriptPath = '$fileName.$ext';
+			if (Paths.exists(scriptPath))
+				level.script = FunkinHScript.fromFile(scriptPath, level.name, ["this"=>level]);
+		}
+
+		return level;
+	}
+
+	function callScript(call:String, ?args:Array<Dynamic>):Null<Dynamic>
+	{
+		if(script != null && script.exists(call))
+			return script.call(call, args);
+
+		return null;
+	}
+
+	public function new(){}
+
+	public var id:String = 'broken';
+	public var bgColor:FlxColor = 0xFFF9CF51;
+	public var index:Int = 0;
+	public var name:String = "PLACEHOLDER";
+	public var asset:String = "storymenu/titles/week1";
+	public var songs:Array<LevelSongData> = [];
+	public var difficulties:Array<String> = ["easy", "normal", "hard"];
+	public var props:Array<LevelPropData> = [];
+
+	/**
+	 * Returns a file path to the title asset
+	 */
+	public function getAsset():String {
+		return callScript("getAsset") ?? asset;
+	}
+
+	/**
+	 * Returns an integer to decide placement of the level
+	 */
+	public function getIndex():Int {
+		return callScript("getIndex") ?? index;
+	}
+
+	/**
+	 * Returns an array of difficulties available to be played for the level
+	 */
+	public function getDifficulties():Array<String>
+	{ 
+		return callScript("getDifficulties") ?? difficulties;
+	}
+
+	/**
+	 * Returns an array of props to show in the story menu
+	 */
+	public function getProps():Array<LevelPropData> {
+		return callScript("getProps") ?? props;
+	}
+
+	/**
+	 * Returns an array of song IDs to be played during the level
+	 */
+	public function getPlaylist(difficultyID:Int = 1):Array<String> 
+		return cast callScript("getPlaylist", [difficultyID]) ?? [for(song in songs)song.songName];
+	
+
+	/**
+	 * Returns an array of song names to be displayed in the story menu
+	 */
+	public function getDisplayedSongs(difficultyID:Int = 1):Array<String>
+		return cast callScript("getDisplayedSongs", [difficultyID]) ?? [for (song in songs) song.displayName ?? song.songName.capitalize()];
+	
+
+	/**
+	 * WIP (still gotta add to freeplay)
+	 * Returns an array of song data to be shown in freeplay. 
+	 */
+	public function getFreeplaySongs():Array<LevelSongData> 
+		return cast callScript("getFreeplaySongs") ??  songs;
+	
+
+	/**
+	 * Returns a LevelTitle object for the story menu
+	 */
+	public function createTitle()
+		return callScript("createTitle") ?? new LevelTitle(0, 0, getAsset());
+	
+
+	/**
+	 * Creates the props for the visuals in the story menu.
+	 * This is usually the main characters of the level (BF, GF, and Opponent)
+	 * Sometimes includes a background in Psych Engine and similar engines
+	 * @param group The group to be populated by props.
+	 * @param bgGroup The background group to be populated by props. This group is automatically layered behind all props and fades when changing levels.
+	 */
+
+	public function populateGroup(group:FlxSpriteGroup, bgGroup:FlxSpriteGroup){
+		if (callScript("prePopulateGroup", [group, bgGroup]) == Globals.Function_Stop)
+			return;
+
+		for(propData in getProps()){
+			var prop = LevelStageProp.buildFromData(propData);
+			var layer = propData.layer.toLowerCase();
+
+			if (layer == 'background' || layer == 'bg')
+				bgGroup.add(prop);
+			else
+				group.add(prop);
+		}
+
+		callScript("postPopulateGroup", [group, bgGroup]);
+	}
+}
+
+// fuck structInit bro im doing it the way i already know
+typedef JSONLevelData = {
+	?id:String,
+	?index:Int,
+	name:String,
+	asset:String,
+	songs:Array<JSONSongData>,
+	?bgColor:String,
+	?difficulties:Array<String>,
+	?props:Array<LevelPropData>
+}
+
+@:structInit
+class LevelSongData {
+	public var displayName(get, default):String = '';
+	function get_displayName(){
+		if(displayName.trim() == '')
+			return songName.capitalize();
+
+		return displayName;
+	}
+	public var songName:String;
+}
+
+// i know its v-slice core but bleeehh :P
+// allows good customization
+
+
+// TODO: Move all of these to a seperate proper Level class to allow for scripting etc
+typedef LevelPropAnimation = {
+	name:String,
+	prefix:String,
+	?looped:Bool,
+	?fps:Int,
+	?indices:Array<Int>,
+	?offset:Array<Float>,
+	?haltsDancing:Bool
+}
+
+typedef LevelPropData = {
+	?template:String,
+	?layer:String, // Used for fading out the background
+	?characterId:Float,
+	?x:Float,
+	?y:Float,
+	graphic:String,
+	?alpha:Float,
+	?scale:Array<Float>,
+	?antialiasing:Bool,
+	?animations:Array<LevelPropAnimation>,
+
+	?danceSequence:Array<String>, // Cycles through this every dance
+	?danceBeat:Float, // What beat to dance on. 0 = disabled
+}
+
+typedef JSONSongData = OneOfTwo<LevelSongData, String>;
