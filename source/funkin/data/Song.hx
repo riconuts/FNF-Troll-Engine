7e273589fce5460f93b2572bc64ca7fa5be190ed
riconuts <riconuts0@gmail.com>
2025-05-03 04:41:20 -0400

    Create BaseSong.hx

--- source/funkin/data/Song.hx
+++ source/funkin/data/Song.hx
@@ -12,6 +12,7 @@ import funkin.states.LoadingState;
 import funkin.states.PlayState;
 import funkin.states.editors.ChartingState;
 import funkin.data.Section.SwagSection;
+import funkin.data.BaseSong;
 import haxe.io.Path;
 import haxe.Json;
 
@@ -95,47 +96,6 @@ typedef SongMetadata =
 
 inline final DEFAULT_CHART_ID = "normal";
 final defaultDifficultyOrdering:Array<String>  = ["easy", "normal", "hard", "erect", "nightmare"];
-
-abstract class BaseSong
-{
-	public final songId:String;
-	public final folder:String = '';
-
-	public function new(songId:String, folder:String = '')
-	{
-		this.songId = songId;
-		this.folder = folder;
-	}
-
-	public function toString()
-		return '$folder:$songId';
-
-	/**
-	 * Returns metadata for the requested chartId. 
-	 * If it doesn't exist, metadata for the default chart is returned instead
-	 * 
-	 * @param chartId The song chart for which you want to request metadata
-	**/
-	abstract public function getMetadata(chartId:String = DEFAULT_CHART_ID):SongMetadata;
-
-	/**
-	 * Returns chart data for the requested chartId. 
-	 * If it doesn't exist, null is returned instead
-	 * 
-	 * @param chartId The song chart for which you want to request chart data
-	**/
-	abstract public function getSwagSong(chartId:String = DEFAULT_CHART_ID):Null<SwagSong>;
-
-	/**
-	 * Returns a path to a file of name fileName that belongs to this song
-	**/
-	abstract public function getSongFile(fileName:String):String;
-
-	/**
-	 * Returns an array of charts available for this song
-	**/
-	abstract public function getCharts():Array<String>;
-}
 
 class Song extends BaseSong
 {
