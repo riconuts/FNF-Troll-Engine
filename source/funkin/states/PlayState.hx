715d9e3ecdea9268a5b269a4574cc8a65b8a08f0
nebulazorua <12458328+nebulazorua@users.noreply.github.com>
2025-05-17 18:55:09 +0800

    move optionschanged call

--- source/funkin/states/PlayState.hx
+++ source/funkin/states/PlayState.hx
@@ -2162,9 +2162,6 @@ class PlayState extends MusicBeatState
 		hud.alpha = ClientPrefs.hudOpacity;
 		hud.changedOptions(options);
 		
-		callOnScripts('optionsChanged', [options]);
-		if (hudSkinScript != null) callScript(hudSkinScript, "optionsChanged", [options]);
-		
 		for(field in playfields){
 /* 			field.noteField.optimizeHolds = ClientPrefs.optimizeHolds; */
 			field.noteField.drawDistMod = ClientPrefs.drawDistanceModifier;
@@ -2202,6 +2199,10 @@ class PlayState extends MusicBeatState
 				}
 			}
 		}
+
+		callOnScripts('optionsChanged', [options]);
+		if (hudSkinScript != null)
+			callScript(hudSkinScript, "optionsChanged", [options]);
 	}
 
 	override function draw(){
