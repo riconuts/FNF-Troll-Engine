19237a5afce8fdd5e9580424aa94045ba44791df
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2025-06-06 20:39:20 -0400

    get accurate memory counter

---
+++ source/funkin/api/Memory.hx
@@ -0,0 +1,18 @@
+package funkin.api;
+
+import cpp.SizeT;
+
+/**
+ * Gets the accurate memory counter
+ * Original C code by David Robert Nadeau
+ * @see https://web.archive.org/web/20190716205300/http://nadeausoftware.com/articles/2012/07/c_c_tip_how_get_process_resident_set_size_physical_memory_use
+ */
+@:buildXml('<include name="../../../../source/funkin/api/build.xml" />')
+@:include("memory.h")
+extern class Memory {
+    @:native("getPeakRSS")
+    static function getPeakRSS():SizeT;
+
+    @:native("getCurrentRSS")
+    static function getCurrentRSS():SizeT;
+}
