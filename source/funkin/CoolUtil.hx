c0abfd99d73edc024be636e088abbbb79dad3640
riconuts <68708956+riconuts@users.noreply.github.com>
2025-11-10 00:11:25 -0300

    Update CoolUtil.hx

--- source/funkin/CoolUtil.hx
+++ source/funkin/CoolUtil.hx
@@ -173,30 +173,31 @@ class CoolUtil {
 		return listFromString(rawList);
 	}
 
-	public static function dominantColor(sprite:flixel.FlxSprite):Int{
-		var countByColor:Map<Int, Int> = [];
-		for(col in 0...sprite.frameWidth) {
-			for(row in 0...sprite.frameHeight) {
-				var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
-				if (colorOfThisPixel != 0){
-					if (countByColor.exists(colorOfThisPixel)) {
-						countByColor[colorOfThisPixel] =  countByColor[colorOfThisPixel] + 1;
-					}else if (countByColor[colorOfThisPixel] != 13520687 - (2*13520687)){
-						countByColor[colorOfThisPixel] = 1;
-					}
+	public static function dominantColor(sprite:flixel.FlxSprite):FlxColor {
+		final alphaTolerance:Int = 10; // 0 - 255
+
+		var colorCount:Map<Int, Int> = [];
+		for (col in 0...sprite.frameWidth) {
+			for (row in 0...sprite.frameHeight) {
+				var pixelColor:FlxColor = sprite.pixels.getPixel32(col, row);
+				if (pixelColor.alpha >= alphaTolerance) {
+					pixelColor.alpha = 255;	
+					colorCount[pixelColor] = colorCount.exists(pixelColor) ? colorCount[pixelColor] + 1 : 1;
 				}
 			}
 		}
-		var maxCount = 0;
-		var maxKey:Int = 0;//after the loop this will store the max color
-		
-		countByColor[0xFF000000] = 0;
-		for (key in countByColor.keys()) {
-			if (countByColor[key] >= maxCount) {
-				maxCount = countByColor[key];
+		colorCount[0xFF000000] = 0;
+
+		var maxCount:Int = 0;
+		var maxKey:Int = 0; // most occurring color
+		for (key in colorCount.keys()) {
+			if (colorCount[key] >= maxCount) {
+				maxCount = colorCount[key];
 				maxKey = key;
 			}
 		}
+
+		colorCount.clear();
 		return maxKey;
 	}
 
