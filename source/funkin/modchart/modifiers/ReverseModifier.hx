229343a80f02a190ca15fcd67d14ab85b5b84fa5
riconuts <68708956+riconuts@users.noreply.github.com>
2025-11-20 20:52:14 -0300

    incomingAngle modifier
    Rotates the incoming notes direction
    
    Update ReverseModifier.hx

--- source/funkin/modchart/modifiers/ReverseModifier.hx
+++ source/funkin/modchart/modifiers/ReverseModifier.hx
@@ -51,6 +51,8 @@ class ReverseModifier extends NoteModifier
 		#end
 	}
 
+	var distanceVec = new Vector3();
+
 	override function getPos(visualDiff:Float, timeDiff:Float, beat:Float, pos:Vector3, data:Int, player:Int, obj:NoteObject, field:NoteField)
 	{
 		var swagOffset = Note.halfWidth + modMgr.vPadding; // maybe vPadding can be a field variable?
@@ -60,7 +62,17 @@ class ReverseModifier extends NoteModifier
 		var centerPercent = getCenterValue(player);		
 		shift = lerp(shift, (FlxG.height * 0.5), centerPercent);
 		
-		pos.y = shift + lerp(visualDiff, -visualDiff, reversePerc);
+		var distance = lerp(visualDiff, -visualDiff, reversePerc);
+
+		distanceVec.setTo(0, distance, 0);
+		VectorHelpers.rotateV3(distanceVec,
+			(getSubmodValue('incomingAngleX', player) + getSubmodValue('incomingAngleX${data}', player)) * FlxAngle.TO_RAD,
+			(getSubmodValue('incomingAngleY', player) + getSubmodValue('incomingAngleY${data}', player)) * FlxAngle.TO_RAD,
+			(getSubmodValue('incomingAngleZ', player) + getSubmodValue('incomingAngleZ${data}', player)) * FlxAngle.TO_RAD,
+			distanceVec
+		);
+		pos.y = shift;
+		pos.add(distanceVec, pos);
 
 		if ((obj.objType == NOTE))
 		{
@@ -74,10 +86,13 @@ class ReverseModifier extends NoteModifier
 	}
 
 	override function getSubmods(){
-		var subMods:Array<String> = ["cross", "split", "alternate", "centered", "unboundedReverse"];
+		var subMods:Array<String> = ["cross", "split", "alternate", "centered", "unboundedReverse", "incomingAngleX", "incomingAngleY", "incomingAngleZ"];
 
 		for (i in 0...PlayState.keyCount){
 			subMods.push('reverse${i}');
+			subMods.push('incomingAngleX${i}');
+			subMods.push('incomingAngleY${i}');
+			subMods.push('incomingAngleZ${i}');
 		}
 
 		return subMods;
